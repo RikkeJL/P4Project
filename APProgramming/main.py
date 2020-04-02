06ae86c339bd06eecf24a1b00bc6d688da503413
RikkeLyngbjerg <rlyngb18@student.aau.dk>
2020-04-02 16:19:51 +0200

    Added test sound, does not work :P

--- APProgramming/main.py
+++ APProgramming/main.py
@@ -4,8 +4,9 @@ import sounddevice as sd
 import scipy.io.wavfile as wave
 
 sf, soundInput = wave.read('A_Light_Breeze_from_South_West.wav')
-delay = np.int(np.round(0.15*sf))
-echo = echoEffect(soundInput, 0.5, delay)
 
-sd.play(echo, sf)
+#delay = np.int(np.round(0.15*sf))
+#echo = echoEffect(soundInput, 0.5, delay)
+
+sd.play(soundInput, sf)
 status = sd.wait()
