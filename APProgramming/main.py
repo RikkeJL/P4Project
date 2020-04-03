c2ab7d2a43533b98baa588a0b564cf09fb27dceb
RikkeLyngbjerg <rlyngb18@student.aau.dk>
2020-04-03 14:28:04 +0200

    Added Vibrato and tested, shit work :P

--- APProgramming/main.py
+++ APProgramming/main.py
@@ -1,64 +1,28 @@
-<<<<<<< Updated upstream
-from frequencyChange import*
-=======
->>>>>>> Stashed changes
-from echo import*
-from chorus import*
 from vibrato import*
-import pyaudio
-import wave as wv
-import scipy.io.wavfile as waves
+import sounddevice as sd
+import scipy.io.wavfile as wave
 
-filename = 'A_Light_Breeze_from_South_West.wav'
-
-# Set chunk size of 1024 samples per data frame
-chunk = 1024
-
-# Open the sound file
-
-wf = wave.open(filename, 'rb')
-
-# delay = np.int(np.round(0.15*wf.getframerate()))
-# echo = echoEffect(, 0.5, delay)
-# Create an interface to PortAudio
-p = pyaudio.PyAudio()
-
-# Open a .Stream object to write the WAV file to
-# 'output = True' indicates that the sound will be played rather than recorded
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-
-<<<<<<< Updated upstream
-# Read data in chunks
-data = wf.readframes(chunk)
-
-# Play the sound by writing the audio data to the stream
-while data != '':
-    stream.write(data)
-    data = wf.readframes(chunk)
-
-# Close and terminate the stream
-stream.close()
-p.terminate()
-=======
 # Returns the sampling frequency as well as the raw data from the WAV file
-sf, soundInput = wave.read('test3.wav')
+sf, soundInput = wave.read('A_Light_Breeze_from_South_West.wav')
 
 # Normalises the value by ?????
-soundInput = soundInput[:,0]/2**15
+soundInput = soundInput[:]/2**15
 
+# ------------ CALCULATIONS FOR ECHO ------------ #
 # Calculates the delay
-delay = np.int(np.round(0.15*sf))
+# delay = np.int(np.round(0.15*sf))
 
 # applies the echo filter to the input
-echo = echoEffect(soundInput, 10, delay)
+# echo = echoEffect(soundInput, 10, delay)
+
+# ------------ CALCULATIONS FOR VIBRATO ------------ #
+maxDelay = 0.005*sf  # samples
+digModFreq = 2*np.pi*5/sf  # rad/sample
+vibrato = addVibrato(soundInput, maxDelay, digModFreq)
 
 # Plays the melody based on the raw data and the sampling frequency.
-sd.play(echo, sf)
+sd.play(vibrato, sf)
 
 # Makes sure to not stop the melody before everything has played through.
 status = sd.wait()
->>>>>>> Stashed changes
+
--- APProgramming/vibrato.py
+++ APProgramming/vibrato.py
@@ -1,2 +1,20 @@
 import numpy as np
 
+
+def addVibrato(inputSignal, modDepth, digModFreq, offset=0):
+    nData = np.size(inputSignal)
+    outputSignal = np.zeros(nData)
+    tmpSignal = np.zeros(nData)
+    for n in np.arange(nData):
+        # calculate delay
+        delay = offset + (modDepth/2)*(1-np.cos(digModFreq*n))
+        # calculate filter output
+        if n < delay:
+            outputSignal[n] = 0
+        else:
+            intDelay = np.int(np.floor(delay))
+            tmpSignal[n] = inputSignal[n-intDelay]
+            fractionalDelay = delay-intDelay
+            apParameter = (1-fractionalDelay)/(1+fractionalDelay)
+            outputSignal[n] = apParameter*tmpSignal[n]+tmpSignal[n-1]-apParameter*outputSignal[n-1]
+    return outputSignal
