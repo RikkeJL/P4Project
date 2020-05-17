3103aeb5ae8bafa418866081b4ac14f138fbe142
AlexT <akrach18@student.aau.dk>
2020-05-17 10:51:27 +0200

    Changed the values to match the values from the FSR

--- APProgramming/SoundEffects/frequencyChange.py
+++ APProgramming/SoundEffects/frequencyChange.py
@@ -4,5 +4,4 @@ import numpy as np
 def resampleFreq(data, factor):
     freqchange = np.round(np.arange(0, len(data), factor))
     freqchange = freqchange[freqchange < len(data)]
-    print("Frequency Change Done!")
     return data[freqchange.astype(int)]
