237952c366531aa2fdb5558c2eecfa3ae7413eef
AlexT <akrach18@student.aau.dk>
2020-05-22 10:57:02 +0200

    Commented the frequencyChange code and the ArduinoRead code.

--- APProgramming/SoundEffects/frequencyChange.py
+++ APProgramming/SoundEffects/frequencyChange.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 def resampleFreq(data, factor):
-    freqchange = np.round(np.arange(0, len(data), factor))
-    freqchange = freqchange[freqchange < len(data)]
-    return data[freqchange.astype(int)]
+    freqchange = np.round(np.arange(0, len(data), factor)) #calulates the array for the data with the spacing between
+                                                           #each datapoint as defined by the factor amount.
+                                                           #The np.round is called to avoid spacing by a float number.
+
+    freqchange = freqchange[freqchange < len(data)] #apply the data to the new array.
+    return data[freqchange.astype(int)] #returns the array with the data.
--- APProgramming/UserInput/ArduinoRead.py
+++ APProgramming/UserInput/ArduinoRead.py
@@ -3,23 +3,29 @@ from UserInput.UseData import *
 
 
 def readInput():
-    ser = serial.Serial('COM4', 9600)
+    ser = serial.Serial('COM4', 9600) #tries to connect to the specified serial port,
+                                      #and reads the values in 9600 bits/second.
     state = 0
     lastvalueID = -1
 
     while True:
-        incomingbytes = ser.readline()
-        incomingbytes = incomingbytes.decode()
-        incomingbytes.rstrip()
-        print(incomingbytes)
+        incomingbytes = ser.readline() #reads the specified byte array from the serial port.
+        # The length of the expected line, is equal to the reads speed set in the serial.Serial
 
-        if incomingbytes.find('S') != -1:
-            incomingbytes = incomingbytes.translate({ord('S'): None})
-            incomingstate = int(incomingbytes)
+        incomingbytes = incomingbytes.decode() #decodes the array
+        incomingbytes.rstrip() #removes noise and white spaces from the array
+
+        if incomingbytes.find('S') != -1: #if the array holds a character S in it, then:
+            incomingbytes = incomingbytes.translate({ord('S'): None}) #remove the S from the array.
+            incomingstate = int(incomingbytes) #takes the value and typecast it as an integer.
             if state != incomingstate:
-                state = incomingstate
+                state = incomingstate #if the state is not the same as the incoming state,
+                                      #set state equal to incomingstate
 
-        if incomingbytes.find('F') != -1:
-            incomingbytes = incomingbytes.translate({ord('F'): None})
-            sensorvalue = int(incomingbytes)
-            lastvalueID = soundEffect(lastvalueID, sensorvalue, state)
+
+        if incomingbytes.find('F') != -1: #if the array holds a character F in it, then:
+            incomingbytes = incomingbytes.translate({ord('F'): None}) #remove the F from the array.
+            sensorvalue = int(incomingbytes) #takes the value and typecast it as an integer.
+            lastvalueID = soundEffect(lastvalueID, sensorvalue, state) # Run the function lastvalueID and
+                                                                       # send the lastvalueID, sensorvalue and state.
+                                                                       # The function returns a new lastvalueID
