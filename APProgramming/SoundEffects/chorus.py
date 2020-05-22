8f7be6fc8a83ba61d49e5067738f83454d71eec7
RikkeLyngbjerg <rlyngb18@student.aau.dk>
2020-05-22 09:54:42 +0200

    Finished Comments!

--- APProgramming/SoundEffects/chorus.py
+++ APProgramming/SoundEffects/chorus.py
@@ -1,37 +1,36 @@
 import numpy as np
 
 
-def addVibratoToChorus(inputSignal, sf, digModFreq, changeVal, offset):
-    modDepth = changeVal * sf  # samples
-    nData = np.size(inputSignal)
-    outputSignal = np.zeros(nData)
-    tmpSignal = np.zeros(nData)
-    for n in np.arange(nData):
-        # calculate delay
-        delay = offset + (modDepth/2)*(1-np.cos(digModFreq*n))
+def addVibratoToChorus(inputSignal, modDepth, digModFreq, offset):
+    nData = np.size(inputSignal)  # Gets size of input signal array
+    outputSignal = np.zeros(nData)  # makes an array of length nData purely out of zeros
+    tmpSignal = np.zeros(nData)  # makes another array of length nData purely out of zeros
+    for n in np.arange(nData):  # For all values from 0 to nData
+        delay = offset + (modDepth/2)*(1-np.cos(digModFreq*n))  # calculates time-varying delay
         # calculate filter output
-        if n < delay:
-            outputSignal[n] = 0
+        if n < delay:  # if n is smaller than delay
+            outputSignal[n] = 0  # set outputsignal n to 0
         else:
-            intDelay = np.int(np.floor(delay))
-            tmpSignal[n] = inputSignal[n-intDelay]
-            fractionalDelay = delay-intDelay
-            apParameter = (1-fractionalDelay)/(1+fractionalDelay)
-            outputSignal[n] = apParameter*tmpSignal[n]+tmpSignal[n-1]-apParameter*outputSignal[n-1]
-    return outputSignal
+            intDelay = np.int(np.floor(delay))  # make an integer delay that is rounded down from the float delay
+            tmpSignal[n] = inputSignal[n-intDelay]  # save the temp signal to be the nth - intdelay inputsignal
+            fractionalDelay = delay-intDelay  # make a fractional delay based on the delay and intdelay
+            apParameter = (1-fractionalDelay)/(1+fractionalDelay)  # calculates parameter b for allpass filter
+            outputSignal[n] = apParameter*tmpSignal[n]+tmpSignal[n-1]-apParameter*outputSignal[n-1]  # runs song through
+            # allpass filter
+    return outputSignal  # returns outputsignal array
 
 
 def chorusEffect(inputSignal, changeVal, sf):
-    mixParam = np.array([0.9, 0.9, 0.8])
-    offset = np.array([0.1, 0.12, 0.08])*sf  # samples
-    digModFreq = 2*np.pi*np.array([0.1, 0.15, 0.05])/sf  # radians/sample
-    modDepth = np.array([0.02, 0.021, 0.018])*sf  # samples
-    # add the original instrument to the mix
-    outputSignal = inputSignal
-    # add additional instruments using the vibrato effect
-    nAdditionInstruments = np.size(mixParam)
-    for ii in np.arange(nAdditionInstruments):
+    mixParam = np.array([0.9, 0.9, 0.8])  # sets mix parameters for the k'th simulated song
+    offset = np.array([0.1, 0.12, 0.08])*sf  # sets the offsets
+    digModFreq = 2*np.pi*np.array([0.1, 0.15, 0.05])/sf  # Calculates part of the time varying delay.
+    modDepth = np.array([changeVal, changeVal-0.01, changeVal-0.05])*sf  # Maximum delay in samples
+    outputSignal = inputSignal  # Set output signal to be equal to the input signal
+    nAdditionInstruments = np.size(mixParam)  # make an array of nAdditional Instruments and make it the size of mix
+    # params
+    for ii in np.arange(nAdditionInstruments):  # for all values from 0 to nAdditionInstruments
         outputSignal = outputSignal+mixParam[ii]*addVibratoToChorus(inputSignal,
-                                                                    modDepth[ii], changeVal, digModFreq[ii], offset[ii])
-    print("Chorus Done!")
-    return outputSignal
+                                                                    modDepth[ii], digModFreq[ii], offset[ii])
+        # The inputsignal through vibrato effect, while adding the outputsignal and multiplying it with the mix
+        # parameters
+    return outputSignal  # return output signal
--- APProgramming/SoundEffects/vibrato.py
+++ APProgramming/SoundEffects/vibrato.py
@@ -2,20 +2,20 @@ import numpy as np
 
 
 def addVibrato(inputSignal, changeVal, sf, offset=1):
-    modDepth = changeVal * sf  # samples
-    digModFreq = 2 * np.pi * 5 / sf  # rad/sample
-    nData = np.size(inputSignal)
-    outputSignal = np.zeros(nData)
-    tmpSignal = np.zeros(nData)
-    for n in np.arange(nData):
-        delay = offset + (modDepth/2)*(1-np.cos(digModFreq*n))  # calculate delay
-        # calculate filter output
-        if n < delay:
-            outputSignal[n] = 0
+    modDepth = changeVal * sf  # Maximum delay in samples
+    digModFreq = 2 * np.pi * 5 / sf  # Calculates part of the time varying delay.
+    nData = np.size(inputSignal)  # Gets size of input signal array
+    outputSignal = np.zeros(nData)  # makes an array of length nData purely out of zeros
+    tmpSignal = np.zeros(nData)  # makes another array of length nData purely out of zeros
+    for n in np.arange(nData):  # For all values from 0 to nData
+        delay = offset + (modDepth/2)*(1-np.cos(digModFreq*n))  # calculates time-varying delay
+        if n < delay:  # if n is smaller than delay
+            outputSignal[n] = 0  # set outputsignal n to 0
         else:
-            intDelay = np.int(np.floor(delay))
-            tmpSignal[n] = inputSignal[n-intDelay]
-            fractionalDelay = delay-intDelay
-            apParameter = (1-fractionalDelay)/(1+fractionalDelay)
-            outputSignal[n] = apParameter*tmpSignal[n]+tmpSignal[n-1]-apParameter*outputSignal[n-1]
-    return outputSignal
+            intDelay = np.int(np.floor(delay))  # make an integer delay that is rounded down from the float delay
+            tmpSignal[n] = inputSignal[n-intDelay]  # save the temp signal to be the nth - intdelay inputsignal
+            fractionalDelay = delay-intDelay  # make a fractional delay based on the delay and intdelay
+            apParameter = (1-fractionalDelay)/(1+fractionalDelay)  # calculates parameter b for allpass filter
+            outputSignal[n] = apParameter*tmpSignal[n]+tmpSignal[n-1]-apParameter*outputSignal[n-1]  #runs song through
+            # allpass filter
+    return outputSignal  # returns outputsignal array
